e52f51648fc2c70e253491deee45082a4659a3ae
ChrisAgold <christophersagold@gmail.com>
2021-07-01 16:25:05 -0500

    added style and button

--- features/focus/Focus.js
+++ features/focus/Focus.js
@@ -1,15 +1,16 @@
 import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {TextInput} from "react-native-paper"
+import {RoundedButton} from "../../components/RoundedButton";
 
 export const Focus = () => {
-
     return (
         <View style={styles.container}>
             <View style={styles.titleContainer}>
                 <Text style={styles.title}>What would you like to focus on?</Text>
                 <View style={styles.inputContainer}>
-                    <TextInput/>
+                    <TextInput style={{flex: 1, marginRight: 20}}/>
+                    <RoundedButton size={50} title='+' />
                 </View>
             </View>
         </View>
@@ -31,6 +32,7 @@ const styles = StyleSheet.create({
         fontSize: 15,
     },
     inputContainer: {
-        paddingTop: 20
+        paddingTop: 20,
+        flexDirection: "row",
     }
 })
