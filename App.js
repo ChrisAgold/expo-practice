af09fd56249b90e6b50c629692cf2f43e59348e0
ChrisAgold <christophersagold@gmail.com>
2021-07-02 14:39:10 -0500

    added image screen

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import WelcomeScreen from './screens/WelcomeScreen'
+import ViewImageScreen from './screens/ViewImageScreen'
 import {View} from "react-native";
 
 export default function App() {
     // const [focusSubject, setFocusSubject] = useState(null)
 
     return (
-        <WelcomeScreen/>
+        <ViewImageScreen/>
     );
 }
 
